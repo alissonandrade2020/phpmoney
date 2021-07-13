2c22adf0be1bcf90d486d6702e9e6be549cdb808
alissonandrade2020 <alisson2d@hotmail.com>
2021-07-13 14:25:29 -0300

    Login

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'adm
     //Route::get('deposit', 'BalanceController@deposit')->name('balance.deposit');
     //Route::get('balance', 'BalanceController@index')->name('admin.balance');
 
-    Route::get('admin', 'App\Http\Controllers\Admin\AdminController@index')->name('admin.home');
+    Route::get('/', 'App\Http\Controllers\Admin\AdminController@index')->name('admin.home');
 });
 
-Route::get('/', 'App\Http\Controllers\Site\SiteController@index')->name('home');
+Route::get('/admin', 'App\Http\Controllers\Site\SiteController@index')->name('home');
 
 Auth::routes();
 

7485df2e789c2ddf0c2ffdb58656e163f8b10458
alissonandrade2020 <alisson2d@hotmail.com>
2021-07-13 16:41:16 -0300

    Menu -2

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -3,8 +3,154 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
+use App\User;
 
 class Balance extends Model
 {
-   public $timestamps = false;
+    // private $table = 'balance';
+    public $timestamps = false;
+
+
+    public function deposit(float $value) : Array
+    {
+        DB::beginTransaction();
+
+        $totalBefore = $this->amount ? $this->amount : 0;
+        $this->amount += number_format($value, 2, '.', '');
+        $deposit = $this->save();
+
+        $historic = auth()->user()->historics()->create([
+            'type'          => 'I',
+            'amount'        => $value,
+            'total_before'  => $totalBefore,
+            'total_after'   => $this->amount,
+            'date'          => date('Ymd'),
+        ]);
+
+        if ($deposit && $historic) {
+
+            DB::commit();            
+
+            return [
+                'success' => true,
+                'message' => 'Sucesso ao recarregar'
+            ];
+        } else {
+
+            DB::rollback();
+
+            return [
+                'success' => false,
+                'message' => 'Falha ao carregar'
+            ];
+        }
+    }
+
+
+    public function withdraw(float $value) : Array
+    {
+        if ($this->amount < $value)
+            return [
+                'success' => false,
+                'message' => 'Saldo insuficiênte',
+            ];
+
+
+        DB::beginTransaction();
+
+        $totalBefore = $this->amount ? $this->amount : 0;
+        $this->amount -= number_format($value, 2, '.', '');
+        $withdraw = $this->save();
+
+        $historic = auth()->user()->historics()->create([
+            'type'          => 'O',
+            'amount'        => $value,
+            'total_before'  => $totalBefore,
+            'total_after'   => $this->amount,
+            'date'          => date('Ymd'),
+        ]);
+
+        if ($withdraw && $historic) {
+
+            DB::commit();            
+
+            return [
+                'success' => true,
+                'message' => 'Sucesso ao retirar'
+            ];
+        } else {
+
+            DB::rollback();
+
+            return [
+                'success' => false,
+                'message' => 'Falha ao retirar'
+            ];
+        }
+    }
+
+
+    public function transfer(float $value, User $sender): Array
+    {
+        if ($this->amount < $value)
+            return [
+                'success' => false,
+                'message' => 'Saldo insuficiênte',
+            ];
+
+
+        DB::beginTransaction();
+
+        /********************************************************
+         * Atualiza o próprio saldo
+         * *****************************************************/
+        $totalBefore = $this->amount ? $this->amount : 0;
+        $this->amount -= number_format($value, 2, '.', '');
+        $transfer = $this->save();
+
+        $historic = auth()->user()->historics()->create([
+            'type'                  => 'T',
+            'amount'                => $value,
+            'total_before'          => $totalBefore,
+            'total_after'           => $this->amount,
+            'date'                  => date('Ymd'),
+            'user_id_transaction'   => $sender->id
+        ]);
+
+
+        /********************************************************
+         * Atualiza o saldo do recebedor
+         * *****************************************************/
+        $senderBalance = $sender->balance()->firstOrCreate([]);
+        $totalBeforeSender = $senderBalance->amount ? $senderBalance->amount : 0;
+        $senderBalance->amount += number_format($value, 2, '.', '');
+        $transferSender = $senderBalance->save();
+
+        $historicSender = $sender->historics()->create([
+            'type'                  => 'I',
+            'amount'                => $value,
+            'total_before'          => $totalBeforeSender,
+            'total_after'           => $senderBalance->amount,
+            'date'                  => date('Ymd'),
+            'user_id_transaction'   => auth()->user()->id,
+        ]);
+
+        if ($transfer && $historic && $transferSender && $historicSender) {
+
+            DB::commit();            
+
+            return [
+                'success' => true,
+                'message' => 'Sucesso ao transferir'
+            ];
+        }
+
+        DB::rollback();
+
+        return [
+            'success' => false,
+            'message' => 'Falha ao retirar'
+        ];
+    }
 }
--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -1,11 +1,76 @@
 <?php
 
-namespace App\Models\Models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use App\User;
 
-class \Historic extends Model
+class Historic extends Model
 {
-    use HasFactory;
+    protected $fillable = ['type', 'amount', 'total_before', 'total_after', 'user_id_transaction', 'date'];
+
+
+    public function type($type = null)
+    {
+        $types = [
+            'I' => 'Entrada',
+            'O' => 'Saque',
+            'T' => 'Transferência',
+        ];
+
+        if (!$type)
+            return $types;
+
+        if ($this->user_id_transaction != null && $type == 'I')
+            return 'Recebido';
+        
+        return $types[$type];
+    }
+
+    public function scopeUserAuth($query)
+    {
+        return $query->where('user_id', auth()->user()->id);
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function userSender()
+    {
+        return $this->belongsTo(User::class, 'user_id_transaction');
+    }
+
+
+    public function getDateAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
+
+
+    public function search(Array $data, $totalPage)
+    {
+        $historics = $this->where(function ($query) use ($data) {
+            if (isset($data['id']))
+                $query->where('id', $data['id']);
+
+            if (isset($data['date']))
+                $query->where('date', $data['date']);
+
+            if (isset($data['type']))
+                $query->where('type', $data['type']);
+        })
+        // ->where('user_id', auth()->user()->id)
+        ->userAuth()
+        ->with(['userSender'])
+        ->paginate($totalPage);
+        //->toSql();dd($historics);
+
+        return $historics;
+    }
 }
